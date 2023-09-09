aa95709969b8ee660aae11be8f621893698f5785
Coder_Dheeraj/ <113779503+Dheeraj475@users.noreply.github.com>
2023-09-09 20:18:11 +0530

    Update Authcomp.js

--- src/components/Authcomp.js
+++ src/components/Authcomp.js
@@ -3,19 +3,17 @@ import './Auth.css';
 import { useNavigate } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
+import { DotPulse } from '@uiball/loaders'; // Import the loader component
 
 const Authcomp = () => {
-  // This is a state for showing the animation for singin and singup
   const [isSignIn, setIsSignIn] = useState(true);
+  const [loading, setLoading] = useState(false); // State for tracking loading state
 
-  // Toasting the notification with the various task had done
   const notifyLogin = () => toast.success("Logged in successfully");
   const notifySignup = () => toast.success("Account created successfully");
   const notifyError = () => toast.error("Invalid credentials");
-  const notifyErrorPassword = () => toast.error("Password do not match!");
- 
+  const notifyErrorPassword = () => toast.error("Password does not match!");
 
-  // Toggle between sign-in and sign-up modes
   const toggle = () => {
     setIsSignIn((prevIsSignIn) => !prevIsSignIn);
   };
@@ -23,16 +21,13 @@ const Authcomp = () => {
   useEffect(() => {
     const container = document.getElementById('container');
 
-    // Add 'sign-in' class after 200 milliseconds
     const timeoutId = setTimeout(() => {
       container.classList.add('sign-in');
     }, 200);
 
-    // Clean up the timeout to avoid memory leaks
     return () => clearTimeout(timeoutId);
   }, []);
 
-  // Update the classList based on the state change
   useEffect(() => {
     const container = document.getElementById('container');
     if (isSignIn) {
@@ -50,7 +45,6 @@ const Authcomp = () => {
     }
   }, [isSignIn]);
 
-  // Signup password, and this is all states has it
   const [username, setusername] = useState("");
   const [email, setemail] = useState("");
   const [showPassword, setShowPassword] = useState(false);
@@ -58,7 +52,6 @@ const Authcomp = () => {
   const [password, setPassword] = useState("");
   const [cpassword, setcPassword] = useState("");
 
-  // Making the password visibility when the users clicks on eye icon
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
@@ -67,197 +60,187 @@ const Authcomp = () => {
     setShowcPassword(!showcPassword);
   };
 
-  // Setting the variable that is redirects to the specified pathname
   let navGate = useNavigate();
 
-// Signup, creating the user name email and password through the api call request post
-  const handlesubmit= async (e)=> {
+  const handlesubmit = async (e) => {
     e.preventDefault();
- 
-    if (password === cpassword){
-    const response = await fetch("https://task-note-api.vercel.app/api/auth/createuser" , {
-     method: "post",
-     headers: {
-       'Content-Type':'application/json',
-     },
-     body:JSON.stringify({name:username,email:email, password:password})
-   });
-   const json = await response.json()
-  //  console.log(json)
+    setLoading(true); // Set loading to true when the button is clicked
 
-   if (json.success){
-     localStorage.setItem("token",json.authtoken)
-     notifySignup();
-     navGate("/");
-     
-    } else{
-      // Alert invalid creditionals
-      notifyError();
+    if (password === cpassword) {
+      const response = await fetch("https://task-note-api.vercel.app/api/auth/createuser", {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: username, email: email, password: password })
+      });
+      const json = await response.json();
+
+      if (json.success) {
+        localStorage.setItem("token", json.authtoken);
+        notifySignup();
+        navGate("/");
+      } else {
+        notifyError();
+      }
+    } else {
+      notifyErrorPassword();
     }
-  } else {
-    notifyErrorPassword();
- }
-}
 
+    setLoading(false); // Set loading back to false after the request is complete
+  }
 
-    // Login password
-    const[lemail,setlemail] = useState("")
-    const [showlPassword, setShowlPassword] = useState(false);
-    const [lpassword, setlPassword] = useState("");
-  
-    const togglelPasswordVisibility = () => {
-      setShowlPassword(!showlPassword);
-    };
+  const [lemail, setlemail] = useState("");
+  const [showlPassword, setShowlPassword] = useState(false);
+  const [lpassword, setlPassword] = useState("");
 
+  const togglelPasswordVisibility = () => {
+    setShowlPassword(!showlPassword);
+  };
 
-    const handleLsubmit= async (e)=> {
-     e.preventDefault();
-  
-     const response = await fetch("https://task-note-api.vercel.app/api/auth/login" , {
+  const handleLsubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true); // Set loading to true when the button is clicked
+
+    const response = await fetch("https://task-note-api.vercel.app/api/auth/login", {
       method: "post",
       headers: {
-        'Content-Type':'application/json',
+        'Content-Type': 'application/json',
       },
-      body:JSON.stringify({email:lemail, password:lpassword})
+      body: JSON.stringify({ email: lemail, password: lpassword })
     });
-    const json = await response.json()
-    // console.log(json)
+    const json = await response.json();
 
-    if (json.success){
-      // Save the authtoken and redirect
-      localStorage.setItem("token",json.authtoken)
+    if (json.success) {
+      localStorage.setItem("token", json.authtoken);
       notifyLogin();
       navGate("/");
-
-    } else{
-      // Alert invalid creditionals
+    } else {
       notifyError();
     }
-  
+
+    setLoading(false); // Set loading back to false after the request is complete
   }
 
   return (
     <>
-    <div id="container" className="containerAuth">
-      {/* FORM SECTION */}
-      <div className="row">
-        {/* SIGN UP */}
-        <div className="col align-items-center flex-col sign-up">
-          <div className="form-wrapper align-items-center">
-            <div className="form sign-up">
-            <form onSubmit={handlesubmit}>
-              <h2>Sign up</h2>
-              <div className="input-group">
-              <i className="fa-solid fa-user" style={{color:"#bb00ff"}}></i>
-                <input type="text" name="username" value={username} minLength={3} placeholder="Username" onChange={(e) => setusername(e.target.value)} required/>
-              </div>
-              <div className="input-group">
-              <i className="fa-solid fa-envelope" style={{color:"#bb00ff"}}></i>
-                <input type="email" name="email" value={email} placeholder="Email" onChange={(e) => setemail(e.target.value)} required/>
-              </div>
-              <div className="input-group">
-              <i className="fa-solid fa-key" style={{color:"#bb00ff"}}></i>
-                <input type={showPassword ? "text" : "password"}name="Spassword" value={password} minLength={5} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
+      <div id="container" className="containerAuth">
+        <div className="row">
+          <div className="col align-items-center flex-col sign-up">
+            <div className="form-wrapper align-items-center">
+              <div className="form sign-up">
+                <form onSubmit={handlesubmit}>
+                  <h2>Sign up</h2>
+                  <div className="input-group">
+                    <i className="fa-solid fa-user" style={{ color: "#bb00ff" }}></i>
+                    <input type="text" name="username" value={username} minLength={3} placeholder="Username" onChange={(e) => setusername(e.target.value)} required />
+                  </div>
+                  <div className="input-group">
+                    <i className="fa-solid fa-envelope" style={{ color: "#bb00ff" }}></i>
+                    <input type="email" name="email" value={email} placeholder="Email" onChange={(e) => setemail(e.target.value)} required />
+                  </div>
+                  <div className="input-group">
+                    <i className="fa-solid fa-key" style={{ color: "#bb00ff" }}></i>
+                    <input type={showPassword ? "text" : "password"} name="Spassword" value={password} minLength={5} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
 
-                <span className="password-toggle" onClick={togglePasswordVisibility}>
-                  {showPassword ? (
-                    <i className="fa-solid fa-eye" style={{color:"#bb00ff"}}></i>
-                ) : (
-                    <i className="fa-solid fa-eye-slash" style={{color:"#bb00ff"}}></i>
-                )}
-               </span>
+                    <span className="password-toggle" onClick={togglePasswordVisibility}>
+                      {showPassword ? (
+                        <i className="fa-solid fa-eye" style={{ color: "#bb00ff" }}></i>
+                      ) : (
+                        <i className="fa-solid fa-eye-slash" style={{ color: "#bb00ff" }}></i>
+                      )}
+                    </span>
 
+                  </div>
+                  <div className="input-group">
+                    <i className="fa-solid fa-key" style={{ color: "#bb00ff" }}></i>
+                    <input type={showcPassword ? "text" : "password"} minLength={5} value={cpassword} name="Scpassword" onChange={(e) => setcPassword(e.target.value)} placeholder="Confirm password" required />
+
+                    <span className="password-toggle" onClick={togglecPasswordVisibility}>
+                      {showcPassword ? (
+                        <i className="fa-solid fa-eye" style={{ color: "#bb00ff" }}></i>
+                      ) : (
+                        <i className="fa-solid fa-eye-slash" style={{ color: "#bb00ff" }}></i>
+                      )}
+                    </span>
+
+                  </div>
+                  {loading ? (
+                    <div className="loading-button-container">
+                    <button disabled={true} style={{ background: "#f2f3f8" }}>
+                      <DotPulse size={40} color="#bb00ff" />
+                    </button>
+                  </div>
+                  ) : (
+                    <button>Sign up</button>
+                  )}
+                  <p>
+                    <span> Already have an account? </span>
+                    <b className="pointer" onClick={toggle}>Sign in here </b>
+                  </p>
+                </form>
               </div>
-              <div className="input-group">
-              <i className="fa-solid fa-key" style={{color:"#bb00ff"}}></i>
-                <input type={showcPassword ? "text" : "password"}  minLength={5} value={cpassword} name="Scpassword" onChange={(e) => setcPassword(e.target.value)} placeholder="Confirm password" required/>
+            </div>
+          </div>
 
-                <span className="password-toggle" onClick={togglecPasswordVisibility}>
-                  {showcPassword ? (
-                    <i className="fa-solid fa-eye" style={{color:"#bb00ff"}}></i>
-                ) : (
-                    <i className="fa-solid fa-eye-slash" style={{color:"#bb00ff"}}></i>
-                )}
-               </span>
+          <div className="col align-items-center flex-col sign-in">
+            <div className="form-wrapper align-items-center">
 
+              <div className="form sign-in">
+                <form onSubmit={handleLsubmit}>
+                  <h2>Login</h2>
+                  <div className="input-group">
+                    <i className="fa-solid fa-envelope" style={{ color: "#bb00ff" }}></i>
+                    <input type="email" name="Lemail" value={lemail} placeholder="Email" onChange={(e) => setlemail(e.target.value)} required />
+                  </div>
+                  <div className="input-group">
+                    <i className="fa-solid fa-key" style={{ color: "#bb00ff" }}></i>
+                    <input type={showlPassword ? "text" : "password"} value={lpassword} name="Lpassword" placeholder="Password" minLength={5} onChange={(e) => setlPassword(e.target.value)} required />
+                    <span className="password-toggle" onClick={togglelPasswordVisibility}>
+                      {showlPassword ? (
+                        <i className="fa-solid fa-eye" style={{ color: "#bb00ff" }}></i>
+                      ) : (
+                        <i className="fa-solid fa-eye-slash" style={{ color: "#bb00ff" }}></i>
+                      )}
+                    </span>
+
+                  </div>
+                  {loading ? ( 
+                      <div className="loading-button-container">
+                      <button disabled={true} style={{ background: "#f2f3f8" }}>
+                        <DotPulse size={40} color="#bb00ff" />
+                      </button>
+                    </div>
+                  ) : (
+                    <button>Login</button>
+                  )}
+                  <p>
+                    <span> Don't have an account?</span>
+                    <b className="pointer" onClick={toggle}> Sign up here</b>
+                  </p>
+                </form>
               </div>
-              <button> Sign up </button>
-              <p>
-                <span> Already have an account? </span>
-                <b className="pointer" onClick={toggle}>Sign in here </b>
-              </p>
-            </form>
             </div>
           </div>
         </div>
-        {/* END SIGN UP */}
-        {/* SIGN IN */}
-
-        
-        <div className="col align-items-center flex-col sign-in">
-          <div className="form-wrapper align-items-center">
-            
-            <div className="form sign-in">   
-            <form onSubmit={handleLsubmit}>
-              <h2>Login</h2>
-              <div className="input-group">
-              <i className="fa-solid fa-envelope" style={{color:"#bb00ff"}}></i>
-                <input type="email" name="Lemail" value={lemail} placeholder="Email" onChange={(e) => setlemail(e.target.value)} required/>
-              </div>
-              <div className="input-group">
-                <i className="fa-solid fa-key" style={{color:"#bb00ff"}}></i>
-                <input type={showlPassword ? "text" : "password"} value={lpassword} name="Lpassword" placeholder="Password" minLength={5}  onChange={(e) => setlPassword(e.target.value)} required/>
-                <span className="password-toggle" onClick={togglelPasswordVisibility}>
-                  {showlPassword ? (
-                    <i className="fa-solid fa-eye" style={{color:"#bb00ff"}}></i>
-                ) : (
-                    <i className="fa-solid fa-eye-slash" style={{color:"#bb00ff"}}></i>
-                )}
-               </span>
-
-              </div>
-              <button>Login</button>
-
-
-              <p>
-                <span> Don't have an account?</span>
-                <b className="pointer" onClick={toggle}> Sign up here</b>
-              </p>
-            </form>
+        <div className="row content-row">
+          <div className="col align-items-center flex-col">
+            <div className="text sign-in">
+              <h2>Welcome to TodoNote</h2>
+            </div>
+            <div className="img sign-in">
             </div>
           </div>
-        
+          <div className="col align-items-center flex-col">
+            <div className="img sign-up">
 
-          <div className="form-wrapper">
-
-          </div>
-        </div>
-        
-        {/* END SIGN IN */}
-      </div>
-      {/* END FORM SECTION */}
-      {/* CONTENT SECTION */}
-      <div className="row content-row">
-        {/* SIGN IN CONTENT */}
-        <div className="col align-items-center flex-col">
-          <div className="text sign-in">
-            <h2>Welcome to TodoNote</h2>
-          </div>
-          <div className="img sign-in">
-          </div>
-        </div>
-        {/* END SIGN IN CONTENT */}
-        {/* SIGN UP CONTENT */}
-        <div className="col align-items-center flex-col">
-          <div className="img sign-up">
-
-          </div>
-          <div className="text sign-up">
-            <h2>Join TodoNote</h2>
+            </div>
+            <div className="text sign-up">
+              <h2>Join TodoNote</h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
