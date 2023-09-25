2863584e7fce6d282fe4b2af0eda4902c8644a78
Coder_Dheeraj/ <113779503+Dheeraj475@users.noreply.github.com>
2023-09-25 22:41:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,23 +8,27 @@ import { ToastContainer } from 'react-toastify';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
+  const [selectedPriority, setSelectedPriority] = useState('All');
 
-  const handleSearchChange = (newValue) => {
+  const handleSearchChange = (newValue, newPriority) => {
     setSearchQuery(newValue);
+    setSelectedPriority(newPriority);
   };
+
+  
 
   return (
     <div className="App">
       <>
         <NoteState>
           <BrowserRouter>
-            <Navcomp searchQuery={searchQuery} onSearchChange={handleSearchChange} />
+            <Navcomp searchQuery={searchQuery} onSearchChange={handleSearchChange} selectedPriority={selectedPriority} />
             <ToastContainer position="top-right" style={{ marginTop: "60px" }} />
             <Routes>
               <Route
                 exact
                 path="/"
-                element={<NoteItemcomp searchQuery={searchQuery} />}
+                element={<NoteItemcomp searchQuery={searchQuery} selectedPriority={selectedPriority}  />}
               />
               <Route exact path="/auth" element={<Authcomp />} />
             </Routes>
